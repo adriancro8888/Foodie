024eda5ed8f0b0c9f25decb0b71914867b673bef
adriancro8888 <adriancro8888@gmail.com>
2017-05-23 16:25:18 +0500

    Added trigger to react to changes on label and entry

---
+++ Foodie/Triggers/RequiredValidationTriggerAction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+namespace Foodie
+{
+    public class RequiredValidationTriggerAction : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            sender.BackgroundColor = string.IsNullOrEmpty(sender.Text) ? Color.FromHex("#FFCDD2") : Color.Default;
+        }
+
+    }
+}
